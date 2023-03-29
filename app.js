5485ba92a283305acadc56290c62d7952d910256
MounicaPadisala <117354298+MounicaPadisala@users.noreply.github.com>
2023-03-29 23:40:21 +0100

    loan calculator

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
-document.querySelector('.clear').addEventListener('click', clearJoke);
-
-//create getJokes function
-function getJokes(e) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `https://api.chucknorris.io/jokes/random`, true);
- 
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      //console.log(response);
-      const joke = document.querySelector('.joke');
-      const col = document.createElement('li');
-      joke.appendChild(col);
-      col.appendChild(document.createTextNode(response.value));
-      
-    }
-  
-}
-  xhr.send();
+// Listen for Submit
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  //hide results
+  document.getElementById('results').style.display = 'none';
+  //show results
+  document.getElementById('loading').style.display = 'block';
+  //set time
+  setTimeout(calculateResults, 2000);
 
   e.preventDefault();
-  
+});
+//add fuction calculateResults
+function calculateResults() {
+  console.log('calculating..');
+  //UI vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayments = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  //compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  if (isFinite(monthly)) {
+    console.log('enter if...')
+    monthlyPayments.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
+    //show result
+    document.getElementById('results').style.display = 'block';
+    
+  } else {
+    showError('please check your numbers');
+  }
+ 
 }
-//create clearJokes
-function clearJoke() {
-  col.remove();
+// funciton showErrors
+function showError(error) {
+   //hide loader
+    document.getElementById('loading').style.display = 'none';
+    //hide result
+    document.getElementById('results').style.display = 'none';
+//create Div
+  const errorDiv = document.createElement('div');
+  //get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  //Add class
+  errorDiv.className = 'alert alert-danger';
+  // create textNode and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  //insert error before heading
+  card.insertBefore(errorDiv, heading);
+
+  //clear error after 3 seconds
+  setTimeout(clearError, 3000);
+
+
+}
+// create function clearError
+function clearError() {
+  document.querySelector(".alert").remove();
 }
